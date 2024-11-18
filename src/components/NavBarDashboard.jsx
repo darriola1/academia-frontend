c23ccd56efb41f7f3496df6451668b0753aa8e4b
darriola1 <denn.arr17@gmail.com>
2024-11-18 00:46:01 -0300

    addLogoutButton

--- src/components/NavBarDashboard.jsx
+++ src/components/NavBarDashboard.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { BookOpen } from 'lucide-react';
+import { BookOpen, LogOut } from 'lucide-react';
+import ButtonLogOut from './ui/ButtonLogOut';
+
 
 export default function NavBarDashboard() {
     return (
-        <div className="min-h-screen bg-gradient-to-b text-white">
+        <div className="min-h-screen text-white">
             <div className="fixed left-0 top-0 bottom-0 w-64 bg-gray-800 p-4">
+                {/* <div> */}
                 <div className="flex items-center mb-8">
                     <BookOpen className="h-8 w-8 text-blue-400 mr-2" />
                     <h1 className="text-xl font-bold">Academia de Inglés</h1>
                 </div>
+                <nav>
+                    <ul className="space-y-2">
+
+                        <ButtonLogOut variant="ghost" size="icon">
+
+                        </ButtonLogOut>
+                    </ul>
+                </nav>
             </div>
         </div>
 
